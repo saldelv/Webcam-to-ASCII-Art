87fb5408636de754a2f70fbd8e33e2f2a1bbaf96
Salvatore Del Vecchio <96501610+saldelv@users.noreply.github.com>
2024-07-31 15:09:47 -0400

    Pixel value matrix

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ from PIL import Image
 from PIL import ImageFile
 import matplotlib.pyplot as plt
 
-image = Image.open('test.jpg')
+# open image
+image = Image.open('test.jpg') 
 
+# show image before converting
 plt.imshow(image)
 plt.show()
 
+# get width and height
 w, h = image.size
 print(w)
 print(h)
+
+# get pixel values
+pixels = []
+px = image.load()
+
+for i in range(h):
+    row = []
+    for j in range(w):
+        row.append(px[j, i])
+    pixels.append(row)
+print(pixels)
